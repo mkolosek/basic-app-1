265d40d1bf83cad6b1afdf4707d48351febae254
Kolosek <office@kolosek.com>
2023-03-04 17:25:52 +0100

    healthcheck

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,7 @@
+class HealthController < ApplicationController
+  rescue_from(Exception) { render head: 503 }
+
+  def show
+    render head: 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'health/show'
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/up', to: 'health#show'
 end
