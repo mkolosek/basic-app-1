b57186e5f271924ee51e4261e94b1425e96bef06
Marko M <marko.milosevic@kolosek.com>
2023-05-15 16:16:37 +0200

    Update hard_job_spec.rb

--- spec/sidekiq/hard_job_spec.rb
+++ spec/sidekiq/hard_job_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 RSpec.describe HardJob, type: :job do
   it "does a long test" do
-    sleep 5
+    sleep 10
+    expect(1).to eq(1)
+  end
+  
+  it "does second long test" do
+    sleep 10
+    expect(1).to eq(1)
+  end
+  
+  it "does third long test" do
+    sleep 10
+    expect(1).to eq(1)
+  end
+  
+  it "does forth long test" do
+    sleep 10
     expect(1).to eq(1)
   end
 end
