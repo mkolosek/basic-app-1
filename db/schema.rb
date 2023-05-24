f49c7c4bf96f988cd2be88515d182992b1f6c8fb
mkolosek <marko@kolosek.com>
2023-05-24 12:03:19 +0200

    Schema error test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_27_232658) do
+=======
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_232658) do
+>>>>>>> [ACOMP-898] feat: prepare counter and integration log for classroom schdedules importer
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
