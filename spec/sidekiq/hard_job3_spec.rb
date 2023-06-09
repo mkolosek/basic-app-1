ebeae6a1641460106533229c8418e3d4970bd4c6
mkolosek <marko@kolosek.com>
2023-06-09 13:58:58 +0200

    Speed up tests

--- spec/sidekiq/hard_job3_spec.rb
+++ spec/sidekiq/hard_job3_spec.rb
@@ -1,22 +1,18 @@
 require 'rails_helper'
 RSpec.describe HardJob3, type: :job do
   it "does a long test" do
-    sleep 2
     expect(1).to eq(1)
   end
   
   it "does second long test" do
-    sleep 2
     expect(1).to eq(1)
   end
   
   it "does third long test" do
-    sleep 4
     expect(1).to eq(1)
   end
   
   it "does forth long test" do
-    sleep 1
     expect(1).to eq(1)
   end
 end
